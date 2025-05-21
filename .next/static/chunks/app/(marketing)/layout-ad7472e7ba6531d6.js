87bb21d0a3e97038357660196d77ddef75e17e97
aelshekeil <57611650+aelshekeil@users.noreply.github.com>
2025-05-22 01:50:42 +0800

    deleted

--- .next/static/chunks/app/(marketing)/layout-ad7472e7ba6531d6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[181],{6009:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},6093:()=>{},8082:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,6009,23)),Promise.resolve().then(l.t.bind(l,6093,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[308,379,353,358],()=>s(8082)),_N_E=e.O()}]);
