85b22fd6a7fc83cd38c9b058dbc00212bcb8d217
aelshekeil <57611650+aelshekeil@users.noreply.github.com>
2025-05-22 18:50:09 +0800

    types.ts

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,3 +1,5 @@
+import type { RegisterResponse } from './types';
+
 async function fetchRegisterData(): Promise<RegisterResponse> {
   const response = await fetch("/api/register");
   const data = await response.json() as RegisterResponse;
