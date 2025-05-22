f973de368645b1cd532434bbc5be0e57ba091ce2
aelshekeil <57611650+aelshekeil@users.noreply.github.com>
2025-05-22 13:53:00 +0800

    main

--- src/context/MyContext.tsx
+++ src/context/MyContext.tsx
@@ -1,18 +1,30 @@
-// src/context/MyContext.tsx
-'use client';
+"use client";
 
-import { createContext, useContext, ReactNode, useState } from 'react';
+import { createContext, useContext, ReactNode, useState } from "react";
 
 type ContextType = {
-  testValue: string;
+  userData: any;
+  setUserData: (data: any) => void;
 };
 
-const MyContext = createContext<ContextType>({ testValue: 'default' });
+const MyContext = createContext<ContextType | null>(null);
 
+// Export the provider
 export function ContextProvider({ children }: { children: ReactNode }) {
+  const [userData, setUserData] = useState(null);
+  
   return (
-    <MyContext.Provider value={{ testValue: 'test' }}>
+    <MyContext.Provider value={{ userData, setUserData }}>
       {children}
     </MyContext.Provider>
   );
+}
+
+// Export the custom hook
+export function useMyContext() {
+  const context = useContext(MyContext);
+  if (!context) {
+    throw new Error("useMyContext must be used within ContextProvider");
+  }
+  return context;
 }
