d3fe9b7410a7e48b70100cc95a682e292716a1e8
aelshekeil <57611650+aelshekeil@users.noreply.github.com>
2025-05-22 21:20:08 +0800

    layout-updates

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React from 'react';
-import '../globals.css';
+import '../../globals.css'
 import { ContextProvider } from '@/context/MyContext';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
